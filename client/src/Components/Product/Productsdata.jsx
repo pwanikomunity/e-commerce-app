177c8c455da8f94d1db6bca6ef7d96385983d6c8
Jobwafula <jobwafulabg@gmail.com>
2023-11-14 18:18:45 +0300

    styling products  changes

--- client/src/Components/Product/Productsdata.jsx
+++ client/src/Components/Product/Productsdata.jsx
@@ -12,40 +12,51 @@ export const Productsdata = () => {
   }
   return (
     <>
-      <h1 className='text-center text-info  text-sky-500 dark:text-sky-400'>Happy Shopping</h1>
-      <div className='container-fluid mx-2'>
-        <div className="row mt-5 mx-2 flex ">
-          <div className="col-md-6 ">
-            <button className='btn btn-primary h-8  mb-4' onClick={() => setData(Categories)}>All</button><br />
-            <button className='btn btn-primary h-8 mb-4' onClick={() => filterItems('Men')}>Men</button><br />
-            <button className='btn btn-primary h-8 mb-4' onClick={() => filterItems('women')}>Women</button><br />
-            <button className='btn btn-primary h-8 mb-4' onClick={() => filterItems('children')}>Children</button><br />
-            <button className='btn btn-primary h-8 mb-4' onClick={() => filterItems('Phones')}>Phones</button><br />
-            <button className='btn btn-primary h-8 mb-4' onClick={() => filterItems('Home appliances')}>Home Appliances</button><br />
-            <button className='btn btn-primary h-8 mb-4' onClick={() => filterItems('Computers')}>Computers</button><br />
-            <button className='btn btn-primary h-8 mb-4' onClick={() => filterItems('Sportswear')}>Sports Wears</button><br />
+    <div className='flex h-[3rem] ml-[25%] w-[50%] border border-yellow-900 mb-[1rem]'>
+      <input className='w-[100%] h-[100%] text-center' placeholder='search products'/>
+    </div>
+
+
+    <div className=" flex justify-between mb-[1rem]">
+            <button className=' btn-primary w-[5rem]' onClick={() => setData(Categories)}>All</button><br />
+            <button className=' btn-primary  w-[5rem]' onClick={() => filterItems('Men')}>Men</button><br />
+            <button className=' btn-primary  w-[5rem] ' onClick={() => filterItems('women')}>Women</button><br />
+            <button className=' btn-primary  w-[5rem]' onClick={() => filterItems('children')}>Children</button><br />
+            <button className=' btn-primary   w-[5rem]' onClick={() => filterItems('Phones')}>Phones</button><br />
+            <button className=' btn-primary   w-[5rem]' onClick={() => filterItems('Home appliances')}>Appliances</button><br />
+            <button className=' btn-primary  w-[5rem]' onClick={() => filterItems('Computers')}>Computers</button><br />
+            <button className=' btn-primary  w-[5rem]' onClick={() => filterItems('Sportswear')}>Sports </button><br />
           </div>
-          <div className="col-md-9 m-20">
-            <div className="grid grid-cols-3  ">
+        
+      <div className=''>
+        <div className=" flex ">
+          
+
+          <div className="">
+          
+            <div className=" flex justify-between flex-wrap ">
               {data.map((values) => {
                 const { id, title, price, about, image } = values;
                 return (
-                  <>
-                    <div className="grid grid-cols-3 mb-4 w-400">
-                      <div className="card-wrapper w-450 mx-w-455" key={id}>
-                        <div className="card w-450 min-w-250 bg-white p-4 rounded border border-gray-300 shadow-md hover:scale-105 transition-transform duration-300 ease-in-out">
-                          <img src={image} className='card-img-top' alt={`Card ${id}`} />
-                          <div className="card-body">
-                            <h4 className='card-title'>{title}</h4>
-                            <p>{price}</p>
-                            <p className='card-text'>{about}</p>
-                            <button className='btn btn-primary'>ADD TO CART</button>
+                  
+                    <div className=" border  ">
+                      <div className="" key={id}>
+                        <div className="rounded border shadow-md hover:scale-105 transition-transform duration-300 ease-in-out">
+                          <div className='w-[15rem] h-[16rem]'>
+                          <img  className='w-[100%] h-[100%]' src={image} />
+                          </div>
+                          
+                          <div className="  ">
+                            <h4 className='text-[1rem]'>{title}</h4>
+                            <p className='font-bold'>K sh {price}</p>
+                            <p className='text-[1rem]'>{about}</p>
+                            <button className=' btn-primary h-[3rem]'>ADD TO CART</button>
                           </div>
                         </div>
                       </div>
                     </div>
 
-                  </>
+                  
                 )
               })}
 
