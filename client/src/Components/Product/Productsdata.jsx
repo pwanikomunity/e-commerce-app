2a6640bd34015b1ec52f8a881c6cfd05514a0936
Jobwafula <jobwafulabg@gmail.com>
2023-11-15 15:44:29 +0300

    styling changes

--- client/src/Components/Product/Productsdata.jsx
+++ client/src/Components/Product/Productsdata.jsx
@@ -13,22 +13,22 @@ export const Productsdata = () => {
   return (
     <>
     <div className='flex h-[3rem] ml-[25%] w-[50%] border border-yellow-900 mb-[1rem]'>
-      <input className='w-[100%] h-[100%] text-center' placeholder='search products'/>
+      <input className='w-[100%] h-[100%] text-center mb-15' placeholder='search products'/>
     </div>
 
 
-    <div className=" flex justify-between mb-[1rem]">
-            <button className=' btn-primary w-[5rem]' onClick={() => setData(Categories)}>All</button><br />
-            <button className=' btn-primary  w-[5rem]' onClick={() => filterItems('Men')}>Men</button><br />
-            <button className=' btn-primary  w-[5rem] ' onClick={() => filterItems('women')}>Women</button><br />
-            <button className=' btn-primary  w-[5rem]' onClick={() => filterItems('children')}>Children</button><br />
-            <button className=' btn-primary   w-[5rem]' onClick={() => filterItems('Phones')}>Phones</button><br />
-            <button className=' btn-primary   w-[5rem]' onClick={() => filterItems('Home appliances')}>Appliances</button><br />
-            <button className=' btn-primary  w-[5rem]' onClick={() => filterItems('Computers')}>Computers</button><br />
-            <button className=' btn-primary  w-[5rem]' onClick={() => filterItems('Sportswear')}>Sports </button><br />
+    <div className=" flex justify-between mt-[1rem] mb-[1rem] pl-[8rem] pr-[8rem]">
+            <button className=' btn-primary w-[5rem] ml-[2rem]' onClick={() => setData(Categories)}>All</button><br />
+            <button className=' btn-primary  w-[5rem] ml-[2rem]' onClick={() => filterItems('Men')}>Men</button><br />
+            <button className=' btn-primary  w-[5rem] ml-[2rem]' onClick={() => filterItems('women')}>Women</button><br />
+            <button className=' btn-primary  w-[5rem] ml-[2rem]' onClick={() => filterItems('children')}>Children</button><br />
+            <button className=' btn-primary   w-[5rem] ml-[2rem]' onClick={() => filterItems('Phones')}>Phones</button><br />
+            <button className=' btn-primary   w-[5rem] ml-[2rem]' onClick={() => filterItems('shoes')}>Shoes</button><br />
+            <button className=' btn-primary  w-[5rem] ml-[2rem]' onClick={() => filterItems('Computers')}>Computers</button><br />
+            <button className=' btn-primary  w-[5rem] ml-[2rem]' onClick={() => filterItems('Sportswear')}>Sports </button><br />
           </div>
         
-      <div className=''>
+      <div className='p-[8rem]'>
         <div className=" flex ">
           
 
@@ -39,18 +39,18 @@ export const Productsdata = () => {
                 const { id, title, price, about, image } = values;
                 return (
                   
-                    <div className=" border  ">
+                    <div className="   ">
                       <div className="" key={id}>
-                        <div className="rounded border shadow-md hover:scale-105 transition-transform duration-300 ease-in-out">
-                          <div className='w-[15rem] h-[16rem]'>
-                          <img  className='w-[100%] h-[100%]' src={image} />
+                        <div className="rounded border shadow-md hover:scale-105 transition-transform duration-300 ease-in-out ml-10 mb-20 p-1">
+                          <div className='w-[15rem] h-[15rem]'>
+                          <img  className='w-[100%] h-[100%] object-cover' src={image} />
                           </div>
                           
                           <div className="  ">
                             <h4 className='text-[1rem]'>{title}</h4>
                             <p className='font-bold'>K sh {price}</p>
-                            <p className='text-[1rem]'>{about}</p>
-                            <button className=' btn-primary h-[3rem]'>ADD TO CART</button>
+                            <p className='text-[1rem] w-[14rem]'>{about}</p>
+                            <button className=' btn-primary h-[3rem] '>ADD TO CART</button>
                           </div>
                         </div>
                       </div>
