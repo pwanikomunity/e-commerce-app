bb0891bbaf0f081d8c6f09469731890d23456b56
Jobwafula <jobwafulabg@gmail.com>
2023-11-18 03:52:25 +0300

    changes

--- client/src/Components/home/Content.jsx
+++ client/src/Components/home/Content.jsx
@@ -6,10 +6,10 @@ export default function Content() {
     <section className='ml-[4.5rem]'>
         <h1 className='text-white text-[2rem] mb-[2rem]'>Top Deals E-commerce -buy/sell</h1>
         <section className='flex flex-wrap bg-white  p-[1rem] justify-between'>
-            {Categories.slice(5,10).map((product,key)=>{
+            {Categories.slice(5,12).map((product,key)=>{
                 return(<div key={product.Id}>
                     <div className='w-[10rem] h-[10rem]'>
-                        <img  src={product.image}  className='object-cover w-[100%] h-[100%]' alt={product.title}/>
+                        <img  src={product.image}  className='object-cover w-[100%] h-[100%] border-none' alt={product.title}/>
                     </div>
                     <div><p>{product.title}</p></div>
                     <div><p className='font-bold'> K SH {product.price}</p></div>
